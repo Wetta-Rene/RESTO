1b61a56af854c0f2855004cfcee751e3cfc82000
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 20:37:33 +0200

    Update head.php

--- head.php
+++ head.php
@@ -5,7 +5,8 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="OC - Développeur Web P3: Création d'un site internet pour un restaurant, mise en place des menus.">
         <link type="text/css" rel="stylesheet" href="CSS/styles.css" />
         <title>Ohmyfood</title>
   
