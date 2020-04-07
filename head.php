43a79998022de7c367ccd482ad94d14f0b41a210
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 11:35:48 +0200

    Create head.php

---
+++ head.php
@@ -0,0 +1,17 @@
+<?php
+//pour mettre en place une session si besoin///
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link type="text/css" rel="stylesheet" href="CSS/styles.css" />
+        <title>Ohmyfood</title>
+  
+        
+    </head>
+<body>
+    <header>
+        Ohmyfood
+    </header>
