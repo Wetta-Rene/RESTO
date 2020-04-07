a954d7e2d2abdb1d8f2d7bc39e7ce8337207b554
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 11:35:32 +0200

    Create menu-le-chic-a-la-francaise.php

---
+++ menu-le-chic-a-la-francaise.php
@@ -0,0 +1,49 @@
+<?php
+include("head.php");
+?>
+<section id="ContenuPageMenuChic">
+        
+        <h1>Le chic à la française</h1>
+    
+    
+        <div class="contenaireTitre-detailPageMenuSeul">
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Gauche.png" alt="trait" /></div>
+            <div class="element_contenaireTitre-detailPageMenuSeul">Pour se mettre en appétit</div>
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Droite.png" alt="trait" /></div>
+        </div>
+            <div class="contenaireTitre-detailPageMenuSeul">
+                <p class="element-detailPageMenuSeul">Tartare de poulpe acidulé aux zests d'orange</p>
+                <p class="element-detailPageMenuSeul">79€</p> 
+                <p class="element-detailPageMenuSeul">Velouté de légumes d' antan</p>
+                <p class="element-detailPageMenuSeul">57€</p> 
+                <p class="element-detailPageMenuSeul">Soupe à l' oignon revisitée</p>
+                <p class="element-detailPageMenuSeul">57€</p>
+            </div>
+
+        <div class="contenaireTitre-detailPageMenuSeul">
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Gauche.png" alt="trait" /></div>
+            <div class="element_contenaireTitre-detailPageMenuSeul">Plairie des papilles</div>
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Droite.png" alt="trait" /></div>
+        </div>
+            <div class="contenaireTitre-detailPageMenuSeul">
+                <p class="element-detailPageMenuSeul">Coquilles Saint- Jacques accompagnées d'une purée de panais</p>
+                <p class="element-detailPageMenuSeul">95€</p> 
+                <p class="element-detailPageMenuSeul">Magret de canard et parmentier de pommes de terres</p>
+                <p class="element-detailPageMenuSeul">88€</p> 
+                <p class="element-detailPageMenuSeul">Pigeonneau d'Ille et Vilaine sur son lit de gnocchis aux légumes</p>
+                <p class="element-detailPageMenuSeul">175€</p>
+            </div>          
+        <div class="contenaireTitre-detailPageMenuSeul">
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Gauche.png" alt="trait" /></div>
+            <div class="element_contenaireTitre-detailPageMenuSeul">Gourmandises sucrées</div>
+            <div class="element_contenaireTitre-detailPageMenuSeul"><img src="CSS/IMAGES/traitMenu2Droite.png" alt="trait" /></div>
+        </div>
+            <div class="contenaireTitre-detailPageMenuSeul">
+                <p class="element3-detailPageMenuSeul">Pétales de violettes glacés et purée de noisettes</p> 
+                <p class="element3-detailPageMenuSeul">Fondant au chocolat revisité</p>
+                <p class="element3-detailPageMenuSeul">Millefeuille croustillant</p>
+            </div>
+</section>
+<?php
+include("footer.php");
+?>
