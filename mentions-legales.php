56e6bf2998b887d9b9b92220fb05e42b5f0ad51d
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 11:35:30 +0200

    Create mentions-legales.php

---
+++ mentions-legales.php
@@ -0,0 +1,13 @@
+<?php
+include("head.php");
+?>
+<section id="contenairePageMenuSeul">
+    <h1>Mentions légales</h1>
+    <p>
+    Tout ce qui est sur cette page est vrai ! :)
+    </p>
+
+</section>
+<?php
+include("footer.php");
+?>
