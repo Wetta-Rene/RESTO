b24fcda183ccc70f78e5971c09fbbe3a6ea7f2dd
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 11:35:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+include("head.php");
+?>
+<section>
+    <div id="contenuPage">
+        <div class="menu_contenuPage">
+            <a href="menu-la-note-enchantee.php">
+            <img src="CSS/IMAGES/Menus/menu-1b.png" />
+            </a>
+        </div>
+         <div class="menu_contenuPage">
+            <a href="menu-la-note-enchantee.php">
+            Voir le détail du menu...
+            </a>
+        </div>
+         <div class="menu_contenuPage">
+            <a href="menu-le-chic-a-la-francaise.php">
+            <img src="CSS/IMAGES/Menus/menu-2b.png" />
+            </a>
+        </div>
+         <div class="menu_contenuPage">
+            <a class="menu_contenuPageTexte" href="menu-le-chic-a-la-francaise.php">
+            Voir le détail du menu...
+            </a>
+        </div>       
+        <div class="menu_contenuPage">
+            <a href="menu-le-delice-des-papilles.php">
+            <img src="CSS/IMAGES/Menus/menu-3b.png" />
+            </a>
+        </div>
+         <div class="menu_contenuPage">
+            <a href="menu-le-delice-des-papilles.php">
+            Voir le détail du menu...
+            </a>
+        </div>         
+         <div class="menu_contenuPage">
+            <a href="menu-la-palette-du-gout.php">
+            <img src="CSS/IMAGES/Menus/menu-4b.png" />
+            </a>
+        </div>
+         <div class="menu_contenuPage">
+            <a href="menu-la-palette-du-gout.php">
+            Voir le détail du menu...
+            </a>
+        </div>         
+    </div>
+
+    
+    <div id="contenaireBoutonProchainement">
+            <div id="bouton_prochainement">Prochainement<span class="points-progression">...</span></div>
+    </div>
+    
+</section>
+<?php
+include("footer.php");
+?>
