effb65b9d1a107479c3bb8e80e209e77d66c4094
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-07 20:37:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,44 +5,24 @@ include("head.php");
     <div id="contenuPage">
         <div class="menu_contenuPage">
             <a href="menu-la-note-enchantee.php">
-            <img src="CSS/IMAGES/Menus/menu-1b.png" />
-            </a>
-        </div>
-         <div class="menu_contenuPage">
-            <a href="menu-la-note-enchantee.php">
-            Lire le menu...
+            Menu 1b
             </a>
         </div>
          <div class="menu_contenuPage">
             <a href="menu-le-chic-a-la-francaise.php">
             <img src="CSS/IMAGES/Menus/menu-2b.png" />
             </a>
-        </div>
-         <div class="menu_contenuPage">
-            <a class="menu_contenuPageTexte" href="menu-le-chic-a-la-francaise.php">
-            Lire le menu...
-            </a>
-        </div>       
+        </div>      
         <div class="menu_contenuPage">
             <a href="menu-le-delice-des-papilles.php">
             <img src="CSS/IMAGES/Menus/menu-3b.png" />
             </a>
-        </div>
-         <div class="menu_contenuPage">
-            <a href="menu-le-delice-des-papilles.php">
-            Lire le menu...
-            </a>
-        </div>         
+        </div>      
          <div class="menu_contenuPage">
             <a href="menu-la-palette-du-gout.php">
             <img src="CSS/IMAGES/Menus/menu-4b.png" />
             </a>
-        </div>
-         <div class="menu_contenuPage">
-            <a href="menu-la-palette-du-gout.php">
-            Lire le menu...
-            </a>
-        </div>         
+        </div>      
     </div>
 
     
