e5e85e4a9ac2753cbe9e5b05acedabf1adfdbf05
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-09 19:17:49 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div id="contenuFooter">
-        <p class="elementFooter"><a href="mentions-legales.php">Mentions légales</a></p>
-        <p class="elementFooter"><a href="mailto:contact@ohmyfood.fr"> Contactez-nous </a></p>
-    </div>
-</footer>
- 
-</body>
-</html>
